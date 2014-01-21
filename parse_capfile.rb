19af3b7c7c87789cfbd52e929a23818b5cf680f7
Slawomir Zabkiewicz <slawomir@zabkiewicz.net>
2014-01-21 15:50:42 +0100

    Added output file options

--- parse_capfile.rb
+++ parse_capfile.rb
@@ -4,14 +4,15 @@ require File.expand_path(File.dirname(__FILE__) + '/capfile.rb')
 require 'json'
 require 'optparse'
 
-options = {:path => "."}
+options = {:path => ".", :output => "capfile.json"}
 
 OptionParser.new do |opts|
   opts.banner = "Usage: strano.rb [options]"
   opts.on('-p path', '--path=path', 'Application path') { |path| options[:path] = path }
+  opts.on('-o output', '--output=output', 'Output file') { |output| options[:output] = output}
 end.parse!
 
 
 capfile = Capfile.new(options[:path])
 data = {:tasks => capfile.tasks, :stages => capfile.stages }.to_json
-puts data
+File.open(options[:output],"w") { |file| file.write(data) }
