d29f544da699a3d783f83fcc78e9f148caedeb2a
Slawomir Zabkiewicz <slawomir@zabkiewicz.net>
2014-01-22 10:42:01 +0100

    Changed json structure

--- capfile.rb
+++ capfile.rb
@@ -22,39 +22,47 @@ require 'fileutils'
 
     private
 
-    def load(stage)
+    def load(stage)     
       def build_query(stage)
         if stage.nil?
-          return  %W(-f Capfile -Xx -l STDOUT)
+          return %W(-f Capfile -Xx -l STDOUT)
         else 
           return %W(-f Capfile -Xx -l STDOUT #{stage})
         end
       end
-        
+    
       FileUtils.chdir @path do    
         return Capistrano::CLI.parse(build_query(stage)).execute!         
       end     
     end
 
     def cap_tasks      
-      return Hash[@cap.task_list(:all).sort_by(&:fully_qualified_name).map {|task|
-        [task.fully_qualified_name,task.description]
-      }]     
+      @cap.task_list(:all).sort_by(&:fully_qualified_name).map {|task|
+         {
+          :name => task.fully_qualified_name, 
+          :description => task.description, 
+          :brief_description => task.brief_description
+        }
+      }     
     end
 
     def cap_stages
       def filter(variables)
-        return variables.select{|key, name| name.is_a?(String)}
+        variables.select { |key, name| name.is_a?(String) }
       end
 
-      if !@cap.variables[:stages].nil?
-        return Hash[
-          @cap.variables[:stages].map do |stage|   
-            [stage, filter(Capfile.new(@path, stage).variables)]
-          end
-        ]
+      if !@cap.variables[:stages].nil?     
+         @cap.variables[:stages].map do |stage|           
+            {
+              :name => stage,
+              :variables => filter(Capfile.new(@path, stage).variables)
+            }
+          end        
       else
-        return Hash["default",filter(@cap.variables)]
+        {
+          :name => "default", 
+          :variables => filter(@cap.variables)
+        }
       end      
     end
 end
